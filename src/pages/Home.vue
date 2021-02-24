8ee04c74f19901dae84c5bf6c136d4350f551393
Erwin RA <artmxarea@gmail.com>
2021-02-24 11:26:55 +0700

    add description product and modal

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -28,7 +28,12 @@
     </div>
     <div class="container">
       <transition-group name="fadex" class="row row--grid" tag="div">
-        <div v-for="item in filteredProducts" class="col-12 col-sm-6 col-lg-4" :key="item.id">
+        <div
+          v-for="item in filteredProducts"
+          class="col-12 col-sm-6 col-lg-4 erwin-pointer"
+          @click="detailItem(item)"
+          :key="item.id"
+        >
           <div class="product">
             <div class="product__img">
               <img :src="item.foto_url" alt="" />
@@ -36,11 +41,10 @@
             <h3 class="product__title">{{ item.nama }}</h3>
             <div class="discount-erwin">
               <div data-testid="lblProductDiscount#1">{{ item.diskon }} %</div>
-              <div> {{ item.harga | currency  }}
-              </div>
+              <div>{{ item.harga | currency }}</div>
             </div>
             <div>
-            <span class="product__price">{{ (100 - item.diskon)/100*item.harga | currency }}</span>
+              <span class="product__price">{{ (((100 - item.diskon) / 100) * item.harga) | currency }}</span>
             </div>
             <button @click="addToCart(item)" class="product__add" type="button">
               <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
@@ -82,6 +86,37 @@
         </div>
       </transition-group>
     </div>
+    <vue-final-modal
+      :click-to-close="false"
+      v-model="isModalDetail"
+      classes="modal-container"
+      content-class="modal-content"
+    >
+      <button class="header__close" @click="handleModalDetailClose()">
+        <img src="../assets/svg-icons/error.svg" alt="Close icon" />
+      </button>
+      <span class="modal__title">Detail Produk</span>
+      <div class="modal__content">
+        <div class="product__img__detail">
+          <img :src="detailProduct.foto_url" alt="" />
+        </div>
+        <h3 class="product__title">{{ detailProduct.nama }}</h3>
+        <div class="discount-erwin">
+          <div data-testid="lblProductDiscount#1">{{ detailProduct.diskon }} %</div>
+          <div>{{ detailProduct.harga | currency }}</div>
+        </div>
+        <div class="deskripsi">
+          <span class="product__price">{{ (((100 - detailProduct.diskon) / 100) * detailProduct.harga) | currency }}</span>
+        </div>
+         <p class="product__desc" v-html="detailProduct.deskripsi">{{ detailProduct.deskripsi }}</p>
+
+        <div class="modal__action" @click.prevent="tambahKeranjang()">
+          <button class="form__btn" type="button">
+            Tambahkan ke Keranjang
+          </button>
+        </div>
+      </div>
+    </vue-final-modal>
   </section>
 </template>
 
@@ -96,6 +131,10 @@ export default {
   name: 'Home',
   data: () => ({
     cartItems: [],
+    isModalDetail: false,
+    detailProduct: {
+      nama: '',
+    },
     // items: products,
     items: [],
     category: [],
@@ -138,6 +177,16 @@ export default {
       }
       return false;
     },
+    handleModalDetailClose() {
+      this.isModalDetail = false;
+    },
+    tambahKeranjang() {
+      this.addToCart(this.detailProduct);
+    },
+    detailItem(item) {
+      this.isModalDetail = true;
+      this.detailProduct = item;
+    },
     // add items to cart
     addToCart(itemToAdd) {
       // eslint-disable-next-line no-console
@@ -156,7 +205,7 @@ export default {
         formData.append('diskon', itemToAdd.diskon);
         formData.append('stok', itemToAdd.stok);
         formData.append('foto_url', itemToAdd.foto);
-        formData.append('harga', (100 - itemToAdd.diskon) / 100 * itemToAdd.harga);
+        formData.append('harga', ((100 - itemToAdd.diskon) / 100) * itemToAdd.harga);
         formData.append('id', itemToAdd.id);
         formData.append('kategori', itemToAdd.kategori);
         formData.append('nama', itemToAdd.nama);
@@ -177,8 +226,7 @@ export default {
             localStorage.setItem('order_code', user_order_code);
             this.$toaster.success('Barang di tambahkan ke keranjang');
           })
-          .catch(() => {
-          });
+          .catch(() => {});
       } else {
         this.$swal('Silahkan login terlebih dahulu');
       }
@@ -190,3 +238,95 @@ export default {
   },
 };
 </script>
+
+<style lang="css" scoped>
+@import url("https://fonts.googleapis.com/css?family=Roboto+Condensed");
+.container-input {
+  margin-top: 10px;
+}
+label {
+  color: white;
+}
+/* ==== Validation CSS ==== */
+input:required {
+  box-shadow: none;
+}
+/* ==== Modal CSS ==== */
+::v-deep .modal-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0f0f0;
+}
+::v-deep .modal-content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 95%;
+  width: 90%;
+  margin: 0 1rem;
+  padding: 1rem;
+  border-radius: 0.25rem;
+  background: #f0f0f0;
+  webkit-box-shadow: 15px 15px 30px 0px rgb(0 0 0 / 7%), -15px -15px 30px 0px rgb(255 255 255 / 80%);
+  box-shadow: 15px 15px 30px 0px rgb(0 0 0 / 7%), -15px -15px 30px 0px rgb(255 255 255 / 80%);
+}
+::v-deep .modal__title {
+  margin: 0 2rem 0 0;
+  font-size: 2rem;
+  font-weight: 700;
+  color: black;
+}
+::v-deep .modal__content {
+  flex-grow: 1;
+  overflow-y: auto;
+  color: black;
+}
+::v-deep .modal__action {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+}
+::v-deep .modal__close {
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+}
+/* ==== Input CSS ==== */
+.user-box {
+  position: relative;
+  margin-bottom: 1rem;
+}
+.user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #000;
+  border: none;
+  border-bottom: 1px solid #000;
+  outline: none;
+  background: transparent;
+}
+.user-box label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: rgb(143, 143, 143);
+  pointer-events: none;
+  transition: 0.5s;
+}
+.user-box input:focus ~ label,
+.user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: coral;
+  font-size: 12px;
+}
+.user-box input:focus,
+.user-box input:valid {
+  border-bottom: 1px solid coral;
+}
+</style>
